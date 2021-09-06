5bcbea290d255479d9b30448f9395ac85205c67b
Elton Souza <elton.xavier@outlook.com>
2021-09-06 20:42:57 -0300

    Initial Commit

---
+++ Radio/PlayViewController.swift
@@ -0,0 +1,94 @@
+//
+//  PlayViewController.swift
+//  Radio
+//
+//  Created by Elton Souza on 06/09/21.
+//
+
+import UIKit
+import AVFoundation
+
+class PlayViewController: UIViewController {
+    var isLoading = false {
+        didSet {
+            if isLoading {
+                loading.startAnimating()
+            } else {
+                loading.stopAnimating()
+            }
+        }
+    }
+    
+    var playerItem: AVPlayerItem?
+    var player: AVPlayer?
+    
+    @IBOutlet weak var loading: UIActivityIndicatorView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadMusic("http://strm112.1.fm/acountry_mobile_mp3")
+    }
+    
+    func loadMusic(_ stringUrl: String) {
+        isLoading = true
+        guard let url = URL(string: stringUrl) else {
+            print("Erro na URL")
+            isLoading = false
+            return
+        }
+        
+        preparePlayer(with: AVAsset(url: url)) { (success, asset) in
+            guard success, let asset = asset else {
+                print("ERRO AO BAIXAR A MUSICA")
+                self.isLoading = false
+                return
+            }
+            self.isLoading = false
+            self.playerItem = AVPlayerItem(asset: asset)
+            self.player = AVPlayer(playerItem: self.playerItem)
+            self.play()
+        }
+    }
+    
+    func play() {
+        player?.play()
+    }
+    
+    func stop() {
+        player?.pause()
+        player?.seek(to: CMTime.zero)
+    }
+
+    private func preparePlayer(with asset: AVAsset?, completionHandler: @escaping (_ isPlayable: Bool, _ asset: AVAsset?) -> ()) {
+        guard let asset = asset else {
+            completionHandler(false, nil)
+            return
+        }
+        
+        let requestedKey = ["playable"]
+        
+        asset.loadValuesAsynchronously(forKeys: requestedKey) {
+            
+            DispatchQueue.main.async {
+                var error: NSError?
+                
+                let keyStatus = asset.statusOfValue(forKey: "playable", error: &error)
+                if keyStatus == AVKeyValueStatus.failed || !asset.isPlayable {
+                    completionHandler(false, nil)
+                    return
+                }
+                
+                completionHandler(true, asset)
+            }
+        }
+    }
+    
+    
+    @IBAction func onTouchStop(_ sender: Any) {
+        stop()
+    }
+    
+    @IBAction func onTouchPlay(_ sender: Any) {
+        play()
+    }
+}
